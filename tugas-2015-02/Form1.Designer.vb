faf2e5f16f352c349483523cd289b93c9a305646
devintachr <dee17devinta.dc@gmail.com>
2015-10-10 21:58:43 +0700

    kalkulator

--- tugas-2015-02/Form1.Designer.vb
+++ tugas-2015-02/Form1.Designer.vb
@@ -22,9 +22,215 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.hasilbox = New System.Windows.Forms.TextBox()
+        Me.satu = New System.Windows.Forms.Button()
+        Me.dua = New System.Windows.Forms.Button()
+        Me.tiga = New System.Windows.Forms.Button()
+        Me.empat = New System.Windows.Forms.Button()
+        Me.lima = New System.Windows.Forms.Button()
+        Me.enam = New System.Windows.Forms.Button()
+        Me.tujuh = New System.Windows.Forms.Button()
+        Me.delapan = New System.Windows.Forms.Button()
+        Me.sembilan = New System.Windows.Forms.Button()
+        Me.nol = New System.Windows.Forms.Button()
+        Me.plusbutton = New System.Windows.Forms.Button()
+        Me.minbutton = New System.Windows.Forms.Button()
+        Me.clearbutton = New System.Windows.Forms.Button()
+        Me.hasilbutton = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'hasilbox
+        '
+        Me.hasilbox.Font = New System.Drawing.Font("Microsoft Sans Serif", 36.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.hasilbox.Location = New System.Drawing.Point(5, 10)
+        Me.hasilbox.Name = "hasilbox"
+        Me.hasilbox.ReadOnly = True
+        Me.hasilbox.RightToLeft = System.Windows.Forms.RightToLeft.Yes
+        Me.hasilbox.Size = New System.Drawing.Size(274, 62)
+        Me.hasilbox.TabIndex = 0
+        Me.hasilbox.Text = "0"
+        '
+        'satu
+        '
+        Me.satu.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.satu.Location = New System.Drawing.Point(7, 81)
+        Me.satu.Name = "satu"
+        Me.satu.Size = New System.Drawing.Size(56, 53)
+        Me.satu.TabIndex = 1
+        Me.satu.Text = "1"
+        Me.satu.UseVisualStyleBackColor = True
+        '
+        'dua
+        '
+        Me.dua.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.dua.Location = New System.Drawing.Point(76, 81)
+        Me.dua.Name = "dua"
+        Me.dua.Size = New System.Drawing.Size(56, 53)
+        Me.dua.TabIndex = 2
+        Me.dua.Text = "2"
+        Me.dua.UseVisualStyleBackColor = True
+        '
+        'tiga
+        '
+        Me.tiga.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.tiga.Location = New System.Drawing.Point(146, 81)
+        Me.tiga.Name = "tiga"
+        Me.tiga.Size = New System.Drawing.Size(56, 53)
+        Me.tiga.TabIndex = 3
+        Me.tiga.Text = "3"
+        Me.tiga.UseVisualStyleBackColor = True
+        '
+        'empat
+        '
+        Me.empat.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.empat.Location = New System.Drawing.Point(7, 140)
+        Me.empat.Name = "empat"
+        Me.empat.Size = New System.Drawing.Size(56, 53)
+        Me.empat.TabIndex = 4
+        Me.empat.Text = "4"
+        Me.empat.UseVisualStyleBackColor = True
+        '
+        'lima
+        '
+        Me.lima.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lima.Location = New System.Drawing.Point(76, 140)
+        Me.lima.Name = "lima"
+        Me.lima.Size = New System.Drawing.Size(56, 53)
+        Me.lima.TabIndex = 5
+        Me.lima.Text = "5"
+        Me.lima.UseVisualStyleBackColor = True
+        '
+        'enam
+        '
+        Me.enam.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.enam.Location = New System.Drawing.Point(146, 140)
+        Me.enam.Name = "enam"
+        Me.enam.Size = New System.Drawing.Size(56, 53)
+        Me.enam.TabIndex = 6
+        Me.enam.Text = "6"
+        Me.enam.UseVisualStyleBackColor = True
+        '
+        'tujuh
+        '
+        Me.tujuh.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.tujuh.Location = New System.Drawing.Point(7, 199)
+        Me.tujuh.Name = "tujuh"
+        Me.tujuh.Size = New System.Drawing.Size(56, 53)
+        Me.tujuh.TabIndex = 7
+        Me.tujuh.Text = "7"
+        Me.tujuh.UseVisualStyleBackColor = True
+        '
+        'delapan
+        '
+        Me.delapan.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.delapan.Location = New System.Drawing.Point(76, 199)
+        Me.delapan.Name = "delapan"
+        Me.delapan.Size = New System.Drawing.Size(56, 53)
+        Me.delapan.TabIndex = 8
+        Me.delapan.Text = "8"
+        Me.delapan.UseVisualStyleBackColor = True
+        '
+        'sembilan
+        '
+        Me.sembilan.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.sembilan.Location = New System.Drawing.Point(146, 199)
+        Me.sembilan.Name = "sembilan"
+        Me.sembilan.Size = New System.Drawing.Size(56, 53)
+        Me.sembilan.TabIndex = 9
+        Me.sembilan.Text = "9"
+        Me.sembilan.UseVisualStyleBackColor = True
+        '
+        'nol
+        '
+        Me.nol.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.nol.Location = New System.Drawing.Point(76, 258)
+        Me.nol.Name = "nol"
+        Me.nol.Size = New System.Drawing.Size(56, 53)
+        Me.nol.TabIndex = 10
+        Me.nol.Text = "0"
+        Me.nol.UseVisualStyleBackColor = True
+        '
+        'plusbutton
+        '
+        Me.plusbutton.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.plusbutton.Location = New System.Drawing.Point(7, 258)
+        Me.plusbutton.Name = "plusbutton"
+        Me.plusbutton.Size = New System.Drawing.Size(56, 53)
+        Me.plusbutton.TabIndex = 11
+        Me.plusbutton.Text = "+"
+        Me.plusbutton.UseVisualStyleBackColor = True
+        '
+        'minbutton
+        '
+        Me.minbutton.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.minbutton.Location = New System.Drawing.Point(146, 258)
+        Me.minbutton.Name = "minbutton"
+        Me.minbutton.Size = New System.Drawing.Size(56, 53)
+        Me.minbutton.TabIndex = 12
+        Me.minbutton.Text = "-"
+        Me.minbutton.UseVisualStyleBackColor = True
+        '
+        'clearbutton
+        '
+        Me.clearbutton.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.clearbutton.Location = New System.Drawing.Point(216, 81)
+        Me.clearbutton.Name = "clearbutton"
+        Me.clearbutton.Size = New System.Drawing.Size(56, 132)
+        Me.clearbutton.TabIndex = 13
+        Me.clearbutton.Text = "Clear"
+        Me.clearbutton.UseVisualStyleBackColor = True
+        '
+        'hasilbutton
+        '
+        Me.hasilbutton.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.hasilbutton.Location = New System.Drawing.Point(216, 228)
+        Me.hasilbutton.Name = "hasilbutton"
+        Me.hasilbutton.Size = New System.Drawing.Size(56, 79)
+        Me.hasilbutton.TabIndex = 14
+        Me.hasilbutton.Text = "="
+        Me.hasilbutton.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.Text = "Form1"
+        Me.ClientSize = New System.Drawing.Size(284, 319)
+        Me.Controls.Add(Me.hasilbutton)
+        Me.Controls.Add(Me.clearbutton)
+        Me.Controls.Add(Me.minbutton)
+        Me.Controls.Add(Me.plusbutton)
+        Me.Controls.Add(Me.nol)
+        Me.Controls.Add(Me.sembilan)
+        Me.Controls.Add(Me.delapan)
+        Me.Controls.Add(Me.tujuh)
+        Me.Controls.Add(Me.enam)
+        Me.Controls.Add(Me.lima)
+        Me.Controls.Add(Me.empat)
+        Me.Controls.Add(Me.tiga)
+        Me.Controls.Add(Me.dua)
+        Me.Controls.Add(Me.satu)
+        Me.Controls.Add(Me.hasilbox)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "Kalkulator"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
+    Friend WithEvents hasilbox As System.Windows.Forms.TextBox
+    Friend WithEvents satu As System.Windows.Forms.Button
+    Friend WithEvents dua As System.Windows.Forms.Button
+    Friend WithEvents tiga As System.Windows.Forms.Button
+    Friend WithEvents empat As System.Windows.Forms.Button
+    Friend WithEvents lima As System.Windows.Forms.Button
+    Friend WithEvents enam As System.Windows.Forms.Button
+    Friend WithEvents tujuh As System.Windows.Forms.Button
+    Friend WithEvents delapan As System.Windows.Forms.Button
+    Friend WithEvents sembilan As System.Windows.Forms.Button
+    Friend WithEvents nol As System.Windows.Forms.Button
+    Friend WithEvents plusbutton As System.Windows.Forms.Button
+    Friend WithEvents minbutton As System.Windows.Forms.Button
+    Friend WithEvents clearbutton As System.Windows.Forms.Button
+    Friend WithEvents hasilbutton As System.Windows.Forms.Button
 
 End Class
--- tugas-2015-02/Form1.vb
+++ tugas-2015-02/Form1.vb
@@ -1,3 +1,117 @@
 ﻿Public Class Form1
+    Dim nilai1, nilai2 As Integer
+    Dim hasil As Integer
+    Dim operasi As String
 
+
+    Private Sub satu_Click(sender As Object, e As EventArgs) Handles satu.Click
+        If hasilbox.Text = "0" Or
+            hasilbox.Text = nilai1 Then
+            hasilbox.Text = "1"
+        Else : hasilbox.Text = hasilbox.Text & "1"
+        End If
+    End Sub
+
+    Private Sub dua_Click(sender As Object, e As EventArgs) Handles dua.Click
+        If hasilbox.Text = "0" Or
+            hasilbox.Text = nilai1 Then
+            hasilbox.Text = "2"
+        Else : hasilbox.Text = hasilbox.Text & "2"
+        End If
+    End Sub
+
+    Private Sub tiga_Click(sender As Object, e As EventArgs) Handles tiga.Click
+        If hasilbox.Text = "0" Or
+            hasilbox.Text = nilai1 Then
+            hasilbox.Text = "3"
+        Else : hasilbox.Text = hasilbox.Text & "3"
+        End If
+    End Sub
+
+    Private Sub empat_Click(sender As Object, e As EventArgs) Handles empat.Click
+        If hasilbox.Text = "0" Or
+            hasilbox.Text = nilai1 Then
+            hasilbox.Text = "4"
+        Else : hasilbox.Text = hasilbox.Text & "4"
+        End If
+    End Sub
+
+    Private Sub lima_Click(sender As Object, e As EventArgs) Handles lima.Click
+        If hasilbox.Text = "0" Or
+            hasilbox.Text = nilai1 Then
+            hasilbox.Text = "5"
+        Else : hasilbox.Text = hasilbox.Text & "5"
+        End If
+    End Sub
+
+    Private Sub enam_Click(sender As Object, e As EventArgs) Handles enam.Click
+        If hasilbox.Text = "0" Or
+            hasilbox.Text = nilai1 Then
+            hasilbox.Text = "6"
+        Else : hasilbox.Text = hasilbox.Text & "6"
+        End If
+    End Sub
+
+    Private Sub tujuh_Click(sender As Object, e As EventArgs) Handles tujuh.Click
+        If hasilbox.Text = "0" Or
+            hasilbox.Text = nilai1 Then
+            hasilbox.Text = "7"
+        Else : hasilbox.Text = hasilbox.Text & "7"
+        End If
+    End Sub
+
+    Private Sub delapan_Click(sender As Object, e As EventArgs) Handles delapan.Click
+        If hasilbox.Text = "0" Or
+            hasilbox.Text = nilai1 Then
+            hasilbox.Text = "8"
+        Else : hasilbox.Text = hasilbox.Text & "8"
+        End If
+    End Sub
+
+    Private Sub sembilan_Click(sender As Object, e As EventArgs) Handles sembilan.Click
+        If hasilbox.Text = "0" Or
+            hasilbox.Text = nilai1 Then
+            hasilbox.Text = "9"
+        Else : hasilbox.Text = hasilbox.Text & "9"
+        End If
+    End Sub
+
+    Private Sub nol_Click(sender As Object, e As EventArgs) Handles nol.Click
+        If hasilbox.Text = "0" Or
+            hasilbox.Text = nilai1 Then
+            hasilbox.Text = "0"
+        Else : hasilbox.Text = hasilbox.Text & "0"
+        End If
+    End Sub
+
+    Private Sub plusbutton_Click(sender As Object, e As EventArgs) Handles plusbutton.Click
+        operasi = "+"
+        nilai1 = Convert.ToInt32(hasilbox.Text)
+
+
+    End Sub
+
+    Private Sub minbutton_Click(sender As Object, e As EventArgs) Handles minbutton.Click
+        operasi = "-"
+        nilai1 = Convert.ToInt32(hasilbox.Text)
+     
+        
+        
+    End Sub
+
+    Private Sub hasilbutton_Click(sender As Object, e As EventArgs) Handles hasilbutton.Click
+        nilai2 = Convert.ToInt32(hasilbox.Text)
+        If operasi = "+" Then
+            hasil = nilai1 + nilai2
+            hasilbox.Text = Convert.ToString(hasil)
+            nilai1 = Convert.ToInt32(hasilbox.Text)
+        Else : hasil = nilai1 - nilai2
+            hasilbox.Text = Convert.ToString(hasil)
+            nilai1 = Convert.ToInt32(hasilbox.Text)
+        End If
+    End Sub
+
+    Private Sub clearbutton_Click(sender As Object, e As EventArgs) Handles clearbutton.Click
+        hasilbox.Text = "0"
+    End Sub
 End Class
